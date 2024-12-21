e9a804f2fb1f5da0251a83c4efd0466b3b1dbfe8
LEONARDO GHIONDA S333025 <s333025@studenti.polito.it>
2024-12-21 18:57:17 +0100

    sistemato comportamento del telefono

--- Assets/Scripts/PhoneHandler.cs
+++ Assets/Scripts/PhoneHandler.cs
@@ -1,13 +1,23 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering.Universal.Internal;
 
 public class PhoneHandler : MonoBehaviour
 {
 
     private float _startY = -180;
     private float _finalY = 200;
-    [SerializeField] private float _phoneMoveSpeed = 10;
+    private enum State{
+        rising,
+        descending,
+        bottom,
+        top,
+    };
+    private State _state = State.bottom;
     private List<RectTransform> _rectTrans = new List<RectTransform>();
+    [SerializeField] private float _phoneMoveSpeed = 10;
+
     void Start()
     {
         foreach(RectTransform rt in transform.GetComponentInChildren<RectTransform>())
@@ -21,27 +31,69 @@ public class PhoneHandler : MonoBehaviour
 
     void FixedUpdate()
     {
-        //phone pop up
-        if(Input.GetKey(GameKeys.SHOW_PHONE_KEY))
+
+        if(_state == State.bottom || _state == State.top)
+        {
+            return;
+        }
+
+        if(_state ==State.rising)
         {
             foreach(RectTransform rt in _rectTrans) 
             {
                 if(rt.anchoredPosition.y < _finalY)
                 {
-                    rt.anchoredPosition += new Vector2(0, _phoneMoveSpeed);
+                    if(_finalY - rt.anchoredPosition.y < _phoneMoveSpeed)
+                    {
+                        rt.anchoredPosition = new Vector2(rt.anchoredPosition.x, _finalY);
+                    }
+                    else
+                    {
+                        rt.anchoredPosition += new Vector2(0, _phoneMoveSpeed);
+                    }
                 }
             }
         }
-        //phone pop out
         else
         {
             foreach(RectTransform rt in _rectTrans) 
             {
                 if(rt.anchoredPosition.y > _startY)
                 {
-                    rt.anchoredPosition += new Vector2(0, -_phoneMoveSpeed);
+                    if(rt.anchoredPosition.y - _startY < _phoneMoveSpeed)
+                    {
+                        rt.anchoredPosition = new Vector2(rt.anchoredPosition.x, _startY);
+                    }
+                    else
+                    {
+                        rt.anchoredPosition += new Vector2(0, -_phoneMoveSpeed);
+                    }
                 }
             }
         }
+
     }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(GameKeys.SHOW_PHONE_KEY))
+        {
+            switch(_state)
+            {
+                case State.rising:
+                _state = State.descending;
+                break;
+                case State.descending:
+                _state = State.rising;
+                break;
+                case State.bottom:
+                _state = State.rising;
+                break;
+                case State.top:
+                _state = State.descending;
+                break;
+            }
+        }
+    }
+
 }
